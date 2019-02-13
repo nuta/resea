ca8b19da5c954b872e41d1b9197acaca7b59418e
Seiya Nuta <nuta@seiya.me>
2019-02-13 10:10:49 +0900

    kernel: embed an initfs by include_bytes!
    It is more straightforward.

--- kernel/initfs.rs
+++ kernel/initfs.rs
@@ -2,23 +2,12 @@ use crate::utils::{VAddr, PAddr};
 use crate::arch::{PAGE_SIZE, INITFS_BASE};
 use crate::allocator::PAGE_POOL;
 
-global_asm!(r#"
-.globl initfs_image
-.align 0x10000
-initfs_image:
-.incbin "../initfs.bin"
-"#);
-
-extern "C" {
-    static initfs_image: u8;
-}
-
 pub fn pager(addr: VAddr) -> PAddr {
     let page = PAGE_POOL.alloc_or_panic();
     let offset = addr.as_usize() - INITFS_BASE.as_usize();
 
     unsafe {
-        let from = VAddr::from_ptr(&initfs_image as *const u8).add(offset);
+        let from = VAddr::from_ptr(include_bytes!("../initfs.bin")).add(offset);
         page.copy_from(PAGE_SIZE, from, PAGE_SIZE);
     }
 
