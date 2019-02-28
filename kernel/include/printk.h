ced4dbe7cfde6d5ea5db454b4e616a349695fa8f
Seiya Nuta <nuta@seiya.me>
2019-02-28 22:46:08 +0900

    remove `#' prefix from DEBUG_USER

--- kernel/include/printk.h
+++ kernel/include/printk.h
@@ -8,7 +8,7 @@ void printk(const char *fmt, ...);
 
 #define DEBUG(fmt, ...) printk("DEBUG: " fmt "\n", ## __VA_ARGS__)
 #define DEBUG_USER(fmt, ...) \
-    printk("#%s.%d: " fmt "\n", CURRENT->process->name, CURRENT->tid, ## __VA_ARGS__)
+    printk("%s.%d: " fmt "\n", CURRENT->process->name, CURRENT->tid, ## __VA_ARGS__)
 #define INFO(fmt, ...) printk(fmt "\n", ## __VA_ARGS__)
 #define WARN(fmt, ...) printk("WARN: " fmt "\n", ## __VA_ARGS__)
 #define BUG(fmt, ...) do {\
