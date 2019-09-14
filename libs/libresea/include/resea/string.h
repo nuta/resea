c2d9ed7ad6a987270c30c97748e560704acbe544
Seiya Nuta <nuta@seiya.me>
2019-09-14 17:58:20 +0900

    [libresea] implement strlen

--- libs/libresea/include/resea/string.h
+++ libs/libresea/include/resea/string.h
@@ -3,6 +3,7 @@
 
 #include <types.h>
 
+size_t strlen(const char *s);
 int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, size_t len);
 int memcmp(const void *s1, const void *s2, size_t len);
--- libs/libresea/string.c
+++ libs/libresea/string.c
@@ -1,5 +1,14 @@
 #include <resea.h>
 
+size_t strlen(const char *s) {
+    size_t len = 0;
+    while (*s != '\0') {
+        len++;
+        s++;
+    }
+    return len;
+}
+
 int strcmp(const char *s1, const char *s2) {
     while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2) {
         s1++;
