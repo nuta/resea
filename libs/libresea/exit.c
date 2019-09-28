764934e6808a1764cce3ff66e4ffcdb657457822
Seiya Nuta <nuta@seiya.me>
2019-09-28 13:28:40 +0900

    [libresea] Warn when exit() is called

--- libs/libresea/exit.c
+++ libs/libresea/exit.c
@@ -2,6 +2,7 @@
 #include <idl_stubs.h>
 
 NORETURN void exit(int status) {
+    OOPS("Killing the current process...");
     call_runtime_exit_current(1, status);
     WARN("Error: runtime.exit_current returned");
     backtrace();
