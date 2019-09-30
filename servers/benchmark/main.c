e6a93c8c0cd5a1469288982139be935f54378a37
Seiya Nuta <nuta@seiya.me>
2019-09-30 10:11:56 +0900

    [benchmark] Avoid verbose result messages

--- servers/benchmark/main.c
+++ servers/benchmark/main.c
@@ -21,17 +21,34 @@ int main(void) {
     INFO("IPC round-trip overhead");
     cid_t memmgr = 1;
     uint32_t ops = IPC_SEND | IPC_RECV;
+    uint64_t cycles[NUM_ITERS];
     for (int i = 0; i < NUM_ITERS; i++) {
         get_ipc_buffer()->header = MEMMGR_BENCHMARK_NOP_HEADER;
+
         uint64_t start = readcyclecounter();
         error_t err = sys_ipc(memmgr, ops);
-        uint64_t end = readcyclecounter();
         if (err != OK) {
-            WARN("sys_ipc returned an error: %d", err);
+            ERROR("sys_ipc returned an error: %d", err);
         }
-        INFO("Iter #%d: %d cycles", i + 1, end - start);
+        uint64_t end = readcyclecounter();
+
+        cycles[i] = end - start;
     }
 
+    uint64_t sum = 0;
+    STATIC_ASSERT(NUM_ITERS > 0);
+    uint64_t lowest = cycles[0];
+    uint64_t highest = cycles[0];
+    for (int i = 0; i < NUM_ITERS; i++) {
+        sum += cycles[i];
+        lowest = MIN(lowest, cycles[i]);
+        highest = MAX(highest, cycles[i]);
+    }
+
+    uint64_t avg = sum / NUM_ITERS;
+
     INFO("Finished all benchmarks!");
+    INFO("IPC overhead (nop): %d cycles +- %d",
+         avg, MAX(avg - lowest, highest - avg));
     return 0;
 }
