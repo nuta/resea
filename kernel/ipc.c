c64160d9edbfd475483ffa85b2065f735469a34b
Seiya Nuta <nuta@seiya.me>
2019-11-09 00:51:21 +0900

    [kernel] Add a warning message for debugging

--- kernel/ipc.c
+++ kernel/ipc.c
@@ -223,6 +223,7 @@ error_t sys_ipc(cid_t cid, uint32_t syscall) {
                 if (receiver->info->num_pages < num_pages
                     || !is_valid_page_base_addr(page_base_addr)) {
                     receiver->abort_reason = ERR_NEEDS_RETRY;
+                    WARN("invalid page payload or the receiver does not accept it");
                     return ERR_INVALID_PAGE_PAYLOAD;
                 }
 
