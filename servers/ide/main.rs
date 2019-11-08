06e797fbeb66ebadda7263fd7f420718c09acc45
Seiya Nuta <nuta@seiya.me>
2019-11-09 00:51:50 +0900

    [ide] Register itself on the discovery server

--- servers/ide/main.rs
+++ servers/ide/main.rs
@@ -1,5 +1,6 @@
 use resea::error::Error;
 use resea::idl;
+use resea::idl::storage_device::INTERFACE_ID;
 use resea::PAGE_SIZE;
 use resea::message::Page;
 use resea::channel::Channel;
@@ -32,9 +33,26 @@ impl idl::storage_device::Server for Server {
     }
 }
 
+impl idl::server::Server for Server {
+    fn connect(&mut self, _from: &Channel, interface_id: u8) -> Option<Result<Channel, Error>> {
+        assert_eq!(interface_id, INTERFACE_ID);
+        let ch = Channel::create().unwrap();
+        ch.transfer_to(&self.ch).unwrap();
+        Some(Ok(ch))
+    }
+}
+
+impl resea::server::Server for Server {
+}
+
 #[no_mangle]
 pub fn main() {
     info!("starting...");
     let mut server = Server::new();
-    serve_forever!(&mut server, [storage_device]);
+
+    let ch = Channel::create().unwrap();
+    ch.transfer_to(&server.ch).unwrap();
+    idl::discovery::Client::register(&MEMMGR_SERVER, INTERFACE_ID, ch).unwrap();
+
+    serve_forever!(&mut server, [storage_device, server]);
 }
