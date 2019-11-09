df5187f03966255bc3f55607f46480a08b516366
Seiya Nuta <nuta@seiya.me>
2019-11-09 23:05:50 +0900

    Merge color.h into printk.h

--- kernel/include/color.h
+++ kernel/include/color.h
@@ -2,11 +2,5 @@
 #define __COLOR_H__
 
 // TODO: Add ifdef guard "CONFIG_DISABLE_ANSI_COLOR"
-#define COLOR_BOLD "\e[1m"
-#define COLOR_BOLD_RED "\e[1;91m"
-#define COLOR_YELLOW "\e[0;33m"
-#define COLOR_BLUE "\e[0;94m"
-#define COLOR_CYAN "\e[0;96m"
-#define COLOR_RESET "\e[0m"
 
 #endif
