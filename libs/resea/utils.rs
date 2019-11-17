96e9b344ce7e49c0327d69f5772978407cb6263a
Seiya Nuta <nuta@seiya.me>
2019-11-17 18:35:33 +0900

    [libs/resea] Add some tests

--- libs/resea/utils.rs
+++ libs/resea/utils.rs
@@ -19,3 +19,42 @@ pub fn align_down(value: usize, align: usize) -> usize {
 pub fn align_up(value: usize, align: usize) -> usize {
     align_down(value + (align - 1), align)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_strlen() {
+        assert_eq!(unsafe{ strlen(b"\0" as *const u8) }, 0);
+        assert_eq!(unsafe{ strlen(b"abc\0" as *const u8) }, 3);
+    }
+
+    #[test]
+    fn test_cstr_to_str() {
+        assert_eq!(unsafe{ c_str_to_str(b"\0" as *const u8) }, "");
+        assert_eq!(unsafe{ c_str_to_str(b"abc\0" as *const u8) }, "abc");
+    }
+
+    #[test]
+    fn test_align_down() {
+        assert_eq!(align_down(0, 0x1000), 0);
+        assert_eq!(align_down(0x1000, 0x1000), 0x1000);
+        assert_eq!(align_down(0x1001, 0x1000), 0x1000);
+        assert_eq!(align_down(0x1fff, 0x1000), 0x1000);
+        assert_eq!(align_down(0x2000, 0x1000), 0x2000);
+        assert_eq!(align_down(0x123456, 0x1000), 0x123000);
+        assert_eq!(align_down(0x123456, 1), 0x123456);
+    }
+
+    #[test]
+    fn test_align_up() {
+        assert_eq!(align_up(0, 0x1000), 0);
+        assert_eq!(align_up(0x1000, 0x1000), 0x1000);
+        assert_eq!(align_up(0x1001, 0x1000), 0x2000);
+        assert_eq!(align_up(0x1fff, 0x1000), 0x2000);
+        assert_eq!(align_up(0x2000, 0x1000), 0x2000);
+        assert_eq!(align_up(0x123456, 0x1000), 0x124000);
+        assert_eq!(align_up(0x123456, 1), 0x123456);
+    }
+}
