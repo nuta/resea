654a305036430bc80f04e1cd09cd9b10337eb9cc
Seiya Nuta <nuta@seiya.me>
2019-11-27 19:30:07 +0900

    Implement Display for Error

--- libs/resea/result.rs
+++ libs/resea/result.rs
@@ -15,7 +15,29 @@ pub enum Error {
     NeedsRetry = 22,
     NoReply = 23,
     WouldBlock = 24,
-    __NonExhaustive
+    __NonExhaustive,
+}
+
+impl core::fmt::Display for Error {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Error::Ok => { write!(f, "Ok") }
+            Error::AlreadyReceiving => { write!(f, "AlreadyReceiving") }
+            Error::InvalidCId => { write!(f, "InvalidCId") }
+            Error::InvalidSyscall => { write!(f, "InvalidSyscall") }
+            Error::InvalidArg => { write!(f, "InvalidArg") }
+            Error::Unimplemented => { write!(f, "Unimplemented") }
+            Error::UnknownMessage => { write!(f, "UnknownMessage") }
+            Error::TooShort => { write!(f, "TooShort") }
+            Error::InvalidData => { write!(f, "InvalidData") }
+            Error::NotFound => { write!(f, "NotFound") }
+            Error::InvalidHandle => { write!(f, "InvalidHandle") }
+            Error::NeedsRetry => { write!(f, "NeedsRetry") }
+            Error::NoReply => { write!(f, "NoReply") }
+            Error::WouldBlock => { write!(f, "WouldBlock") }
+            _ => { Ok(()) }
+        }
+    }
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
