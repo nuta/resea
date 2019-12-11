324ca8777cd589c00a099064f140d9673520e99a
Seiya Nuta <nuta@seiya.me>
2019-12-11 21:02:46 +0900

    libs/resea: Add crate doc

--- libs/resea/lib.rs
+++ libs/resea/lib.rs
@@ -1,3 +1,63 @@
+//! # Resea Standard Library
+//! 
+//! Resea Standard Library provides essential features including:
+//! 
+//! - Language primitives and core features: [`RefCell<T>`], [`Rc<T>`], [`Option<T>`], etc.
+//! - Collections: [`Vec<T>`], [`VecDeque<T>`], [`HashMap<K, V>`], etc.
+//! - Resea-specific primitives: [`Channel`], [`Message`], [`Page`], etc.
+//! - IPC stubs: [`resea::idl`]
+//! - Useful macros: [`trace!`], [`info!`], [`oops!`], [`mainloop!`] etc.
+//! 
+//! ## Prelude
+//! Use the prelude module to import frequently used stuff:
+//! ```
+//! use resea::prelude::*;
+//! ```
+//! 
+//! ## Resea-specific Modules
+//! - [`allocator`]
+//! - [`backtrace`]
+//! - [`channel`]
+//! - [`idl`]
+//! - [`lazy_static`]
+//! - [`mainloop`]
+//! - [`message`]
+//! - [`page`]
+//! - [`prelude`]
+//! - [`result`]
+//! - [`server`]
+//! - [`thread_info`]
+//! - [`utils`]
+//! 
+//! [`Vec<T>`]: collections/struct.Vec.html
+//! [`VecDeque<T>`]: collections/struct.VecDeque.html
+//! [`HashMap<K, V>`]: collections/struct.HashMap.html
+//! [`RefCell<T>`]: cell/struct.RefCell.html
+//! [`Rc<T>`]: rc/struct.Rc.html
+//! [`Option<T>`]: option/enum.Option.html
+//! [`Channel`]: channel/struct.Channel.html
+//! [`Message`]: message/struct.Message.html
+//! [`Page`]: page/struct.Page.html
+//! [`resea::idl`]: resea/idl/index.html
+//! [`allocator`]: resea/allocator/index.html
+//! [`backtrace`]: resea/backtrace/index.html
+//! [`channel`]: resea/channel/index.html
+//! [`collections`]: resea/collections/index.html
+//! [`idl`]: resea/idl/index.html
+//! [`lazy_static`]: resea/lazy_static/index.html
+//! [`mainloop`]: resea/mainloop/index.html
+//! [`message`]: resea/message/index.html
+//! [`page`]: resea/page/index.html
+//! [`prelude`]: resea/prelude/index.html
+//! [`result`]: resea/result/index.html
+//! [`server`]: resea/server/index.html
+//! [`thread_info`]: resea/thread_info/index.html
+//! [`utils`]: resea/utils/index.html
+
+//! 
+//! 
+//! 
+
 #![no_std]
 #![feature(asm)]
 #![feature(global_asm)]
@@ -18,7 +78,7 @@ pub use alloc_crate::{
 
 pub use core::{
     array, cell, char, clone, cmp, convert, default, hash, i16, i32, i64, i8, isize, marker, mem,
-    num, ops, pin, ptr, u16, u32, u64, u8, usize,
+    option, num, ops, pin, ptr, u16, u32, u64, u8, usize,
 };
 pub mod sync {
     pub use alloc_crate::sync::{Arc, Weak};
