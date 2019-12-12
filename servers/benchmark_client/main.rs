c56098779e5476a5e1d9c28045a887253f9fc97e
Seiya Nuta <nuta@seiya.me>
2019-12-12 10:26:24 +0900

    benchmark: Warm up cache memory in advance

--- servers/benchmark_client/main.rs
+++ servers/benchmark_client/main.rs
@@ -1,13 +1,11 @@
 use resea::prelude::*;
-use resea::arch::{
-    thread_info::get_thread_info, syscall::{SYSCALL_IPC, IPC_SEND, IPC_RECV}
-};
+use resea::arch::syscall::{SYSCALL_IPC, IPC_SEND, IPC_RECV};
 use resea::server::connect_to_server;
 use resea::idl::benchmark;
 use resea::idl::timer;
 
 static KERNEL_SERVER: Channel = Channel::from_cid(2);
-const NUM_ITERS: usize = 500;
+const NUM_ITERS: usize = 1000;
 
 #[inline(always)]
 fn cpu_cycle_counter() -> u64 {
@@ -35,7 +33,7 @@ fn ipc(cid: i32) {
 
 #[no_mangle]
 pub fn main() {
-    warn!("connecting to a benchmark server...");
+    info!("connecting to a benchmark server...");
     let server = connect_to_server(benchmark::INTERFACE_ID)
         .expect("failed to connect to a benchmark server");
 
@@ -43,15 +41,23 @@ pub fn main() {
     let timer_ch = Channel::create().unwrap();
     let timer_client_ch = Channel::create().unwrap();
     timer_client_ch.transfer_to(&timer_ch).unwrap();
-    timer::call_create(&KERNEL_SERVER, timer_client_ch, 3000, 0).unwrap();
+    timer::call_create(&KERNEL_SERVER, timer_client_ch, 1000, 0).unwrap();
     timer_ch.recv().unwrap();
 
     info!("starting benchmark (round-trip IPC)...");
-    let mut results = vec![0; NUM_ITERS];
-    let thread_info = unsafe { get_thread_info() };
+    let mut results = [0; NUM_ITERS];
     let server_cid = server.cid();
+
+    // Warm up.
+    // TODO: Support eager pager call to prevent page faults during the benchmark.
     for i in 0..NUM_ITERS {
-        thread_info.ipc_buffer.header = benchmark::NOP_MSG;
+        let start = cpu_cycle_counter();
+        ipc(server_cid);
+        results[i] = cpu_cycle_counter() - start;
+    }
+
+    info!("starting benchmark (round-trip IPC)...");
+    for i in 0..NUM_ITERS {
         let start = cpu_cycle_counter();
         ipc(server_cid);
         results[i] = cpu_cycle_counter() - start;
