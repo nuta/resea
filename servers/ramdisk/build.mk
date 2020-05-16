b17fb6bcbb56e19a62e0f9c73d3cf52219d946bb
Seiya Nuta <nuta@seiya.me>
2020-05-17 00:06:13 +0900

    ramdisk: Add a sample file to the disk

--- servers/ramdisk/build.mk
+++ servers/ramdisk/build.mk
@@ -6,11 +6,11 @@ $(BUILD_DIR)/user/servers/ramdisk/disk.o: $(BUILD_DIR)/ramdisk.img
 	echo ".data; .align 4096; .global __image, __image_end; __image: .incbin \"$<\"; __image_end:" > $(@:.o=.S)
 	$(CC) $(CFLAGS) -o $@ -c $(@:.o=.S)
 
-$(BUILD_DIR)/ramdisk.img: $(foreach app, $(APPS), $(BUILD_DIR)/user/$(app).elf)
+$(BUILD_DIR)/ramdisk.img:
 	$(PROGRESS) "GEN" $@
 	mkdir -p $(@D)
 	dd if=/dev/zero of=$@.tmp bs=1024 count=4098
 	mformat -i $@.tmp
-	mmd -i $@.tmp /apps
-	for app in $(APPS); do mcopy -i $@.tmp $(BUILD_DIR)/user/$$app.elf ::/apps/$$app; done
+	echo "hello from ramdisk" > $(BUILD_DIR)/hello.txt
+	mcopy -i $@.tmp $(BUILD_DIR)/hello.txt ::/HELLO.TXT
 	mv $@.tmp $@
