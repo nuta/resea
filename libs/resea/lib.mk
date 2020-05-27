1f2998fc468394ea59f63a5e2552afcd6a441cef
Seiya Nuta <nuta@seiya.me>
2020-05-27 13:25:06 +0900

    libs/resea: Remove string

--- libs/resea/lib.mk
+++ libs/resea/lib.mk
@@ -1,2 +1,2 @@
-objs := init.o syscall.o printf.o malloc.o io.o lookup.o string.o map.o handle.o
+objs := init.o syscall.o printf.o malloc.o io.o lookup.o map.o handle.o
 include libs/resea/arch/$(ARCH)/arch.mk
