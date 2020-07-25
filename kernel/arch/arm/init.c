bddad15a83990d2f9f046d1386ba3225036f7843
Seiya Nuta <nuta@seiya.me>
2020-07-25 14:16:29 +0000

    kernel/arm: Initialize .bss section with zeroes

--- kernel/arch/arm/init.c
+++ kernel/arch/arm/init.c
@@ -1,6 +1,7 @@
 #include <types.h>
 #include <main.h>
 #include <printk.h>
+#include <string.h>
 #include "peripherals.h"
 
 void arch_idle(void) {
@@ -9,7 +10,11 @@ void arch_idle(void) {
     }
 }
 
+extern char __bss[];
+extern char __bss_end[];
+
 void arm_init(void) {
+    memset(__bss, 0, (vaddr_t) __bss_end - (vaddr_t) __bss);
     arm_peripherals_init();
     kmain();
 
