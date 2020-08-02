be65c58cbe303a09217144e79038d6c82a645ab2
Seiya Nuta <nuta@seiya.me>
2020-08-02 11:41:25 +0000

    tarfs: Create the non-existent output directory

--- servers/fs/tarfs/build.mk
+++ servers/fs/tarfs/build.mk
@@ -5,6 +5,7 @@ $(BUILD_DIR)/servers/fs/tarfs/tarball.o:
 	if [ -z "$(TARFS_TAR)"  ]; then echo "TARFS_TAR is not defined. Enable Linux ABI Compability Layer."; exit 1; fi
 	make $(TARFS_TAR)
 	$(PROGRESS) "GEN" $@
+	mkdir -p $(dir $(@:.o=.S))
 	echo ".data; .align 4096; .global __tarball, __tarball_end; __tarball: .incbin \"$(TARFS_TAR)\"; __tarball_end:" > $(@:.o=.S)
 	$(CC) $(CFLAGS) -o $@ -c $(@:.o=.S)
 
