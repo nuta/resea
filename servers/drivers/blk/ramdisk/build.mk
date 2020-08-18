d11001f9d7732102f12fdea644a1ffc03d998265
Seiya Nuta <nuta@seiya.me>
2020-08-18 14:54:05 +0000

    ramdisk: Fix build.mk

--- servers/drivers/blk/ramdisk/build.mk
+++ servers/drivers/blk/ramdisk/build.mk
@@ -1,7 +1,7 @@
 name := ramdisk
 obj-y := main.o disk.o
 
-$(BUILD_DIR)/servers/ramdisk/disk.o: $(BUILD_DIR)/ramdisk.img
+$(BUILD_DIR)/servers/drivers/blk/ramdisk/disk.o: $(BUILD_DIR)/ramdisk.img
 	$(PROGRESS) "GEN" $@
 	echo ".data; .align 4096; .global __image, __image_end; __image: .incbin \"$<\"; __image_end:" > $(@:.o=.S)
 	$(CC) $(CFLAGS) -o $@ -c $(@:.o=.S)
@@ -11,6 +11,6 @@ $(BUILD_DIR)/ramdisk.img:
 	mkdir -p $(@D)
 	dd if=/dev/zero of=$@.tmp bs=1024 count=4098
 	mformat -i $@.tmp
-	echo "hello from ramdisk" > $(BUILD_DIR)/hello.txt
+	echo "Hello from FAT :D" > $(BUILD_DIR)/hello.txt
 	mcopy -i $@.tmp $(BUILD_DIR)/hello.txt ::/HELLO.TXT
 	mv $@.tmp $@
