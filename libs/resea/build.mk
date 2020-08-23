0e43bb3101fa8ec0428d00970f7b769b46837d42
Seiya Nuta <nuta@seiya.me>
2020-08-23 13:05:04 +0900

    libs/resea: Abandon io.h
    Use libs/driver instead.

--- libs/resea/build.mk
+++ libs/resea/build.mk
@@ -1,5 +1,5 @@
 name := resea
-obj-y += init.o printf.o malloc.o io.o handle.o async.o task.o ipc.o timer.o
+obj-y += init.o printf.o malloc.o handle.o async.o task.o ipc.o timer.o
 obj-y += cmdline.o
 global-cflags-y += -I$(dir)/arch/$(ARCH)
 subdir-y += arch/$(ARCH)
