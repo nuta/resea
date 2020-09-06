a90e01524c97e35aad8aad585ef49f2acb23bb8a
Seiya Nuta <nuta@seiya.me>
2020-09-06 22:23:04 +0900

    kernel/example: Add missing functions

--- kernel/arch/example/mp.c
+++ kernel/arch/example/mp.c
@@ -5,11 +5,20 @@ struct cpuvar cpuvar;
 void halt(void) {
 }
 
-void panic_lock(void) {
-}
-
 void mp_start(void) {
 }
 
 void mp_reschedule(void) {
+}
+
+void lock(void) {
+}
+
+void panic_lock(void) {
+}
+
+void unlock(void) {
+}
+
+void panic_unlock(void) {
 }
