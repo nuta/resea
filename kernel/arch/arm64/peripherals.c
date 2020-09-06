7cfc6ddabc4836b3bebae4b7e758390f5194c2b4
Seiya Nuta <nuta@seiya.me>
2020-09-06 21:52:18 +0900

    raspi3: Initialize UART0 propery

--- kernel/arch/arm64/peripherals.c
+++ kernel/arch/arm64/peripherals.c
@@ -3,27 +3,89 @@
 #include "asm.h"
 #include "peripherals.h"
 
-void uart_send(unsigned int c) {
-    while (mmio_read(UART0_FR) & (1 << 5)) {}
-    mmio_write(UART0_DR, c);
+static inline void delay(unsigned clocks) {
+    while (clocks-- > 0) {
+        __asm__ __volatile__("nop");
+    }
 }
 
-char uart_getc() {
-    while (mmio_read(UART0_FR) & (1 << 4)) {}
+/// Performs a mailbox call.
+static void mbox_call(uint8_t ch, const uint32_t *mbox) {
+    ASSERT(IS_ALIGNED((vaddr_t) mbox, 0x10));
+
+    while (mmio_read(MBOX_STATUS) & MBOX_FULL) {
+        delay(1);
+    }
+
+    // Send a message.
+    unsigned int value =
+        (((vaddr_t) mbox) & ~0xf) // The address of the mailbox to be sent.
+        | (ch & 0xf)              // Channel.
+        ;
+    mmio_write(MBOX_WRITE, value);
+
+    // Wait for the response...
+    while (1) {
+        while (mmio_read(MBOX_STATUS) & MBOX_EMPTY) {
+            delay(1);
+        }
+
+        if (value == mmio_read(MBOX_READ)) {
+            break;
+        }
+    }
+}
+
+void uart_init(void) {
+    // Set up the clock.
+    uint32_t __aligned(16) m[9];
+    m[0] = 9*4;                  // The size of the meessage.
+    m[1] = MBOX_CODE_REQUEST;
+    m[2] = MBOX_TAG_SETCLKRATE;  // Set clock rate.
+    m[3] = 12;                   // The value length.
+    m[4] = 8;                    // The clock ID.
+    m[5] = 2;                    // UART clock
+    m[6] = 4000000;              // The clock rate = 4MHz. (TODO: is 4MHz too low?)
+    m[7] = 0;                    // Allow turbo settings.
+    m[8] = MBOX_TAG_LAST;
+    mbox_call(MBOX_CH_PROP, m);
+
+    // Initialize GPIO pins #14 and #15 as alternative function 0 (UART0).
+    mmio_write(GPIO_FSEL1,
+        (mmio_read(GPIO_FSEL1) & ~((0b111 << 12)|(0b111 << 15)))
+        | (0b100 << 12) | (0b100 << 15));
+
+    // See "GPIO Pull-up/down Clock Registers (GPPUDCLKn)" in "BCM2835 ARM Peripherals"
+    // for more details on this initializaiton sequence.
+    mmio_write(GPIO_PUD, 0);
+    delay(150);
+    mmio_write(GPIO_PUDCLK0, (1 << 14) | (1 << 15));
+    delay(150);
+    mmio_write(GPIO_PUDCLK0, 0);
+
+    mmio_write(UART0_CR, 0);            // Disable UART.
+    mmio_write(UART0_ICR, 0x7ff);       // Disable interrupts from UART.
+    mmio_write(UART0_IBRD, 2);          // baud rate = 115200
+    mmio_write(UART0_FBRD, 11);         //
+    mmio_write(UART0_LCRH, 0b11 << 5);  // 8n1
+    mmio_write(UART0_CR, 0x301);        // Enable RX, TX, and UART0.
+}
+
+bool kdebug_is_readable(void) {
+    return (mmio_read(UART0_FR) & (1 << 4)) == 0;
+}
+
+int kdebug_readchar(void) {
+    if (!kdebug_is_readable()) {
+        return -1;
+    }
+
     return mmio_read(UART0_DR);
 }
 
 void arch_printchar(char ch) {
-    uart_send(ch);
-}
-
-char kdebug_readchar(void) {
-    return '\0'; // TODO:
-}
-
-bool kdebug_is_readable(void) {
-    // TODO: Not yet implemented.
-    return false;
+    while (mmio_read(UART0_FR) & (1 << 5));
+    mmio_write(UART0_DR, ch);
 }
 
 void arm64_timer_reload(void) {
@@ -40,6 +102,7 @@ static void timer_init(void) {
 }
 
 void arm64_peripherals_init(void) {
+    uart_init();
     timer_init();
 }
 
--- kernel/arch/arm64/peripherals.h
+++ kernel/arch/arm64/peripherals.h
@@ -5,9 +5,42 @@
 
 // https://www.raspberrypi.org/documentation/hardware/raspberrypi/bcm2835/BCM2835-ARM-Peripherals.pdf
 // TODO: Let me know if you know the location of the BCM2837 datasheet!
-#define UART0_BASE 0x3f201000
-#define UART0_DR   (UART0_BASE + 0x0000)
-#define UART0_FR   (UART0_BASE + 0x0018)
+
+#define MMIO_BASE      0x3f000000
+#define UART0_DR        (MMIO_BASE + 0x00201000)  // Data register.
+#define UART0_FR        (MMIO_BASE + 0x00201018)  // Flag register.
+#define UART0_IBRD      (MMIO_BASE + 0x00201024)  // Integer baud rate divisor.
+#define UART0_FBRD      (MMIO_BASE + 0x00201028)  // Fractional part of the baud rate divisor.
+#define UART0_LCRH      (MMIO_BASE + 0x0020102c)  // Line control registe.
+#define UART0_CR        (MMIO_BASE + 0x00201030)  // Control register.
+#define UART0_ICR       (MMIO_BASE + 0x00201044)  // Interrupt clear register.
+#define GPIO_FSEL1      (MMIO_BASE + 0x00200004)  // GPIO Function Select 1.
+#define GPIO_PUD        (MMIO_BASE + 0x00200094)  // GPIO Pin Pull-up/down Enable.
+#define GPIO_PUDCLK0    (MMIO_BASE + 0x00200098)  // GPIO Pin Pull-up/down Enable Clock 0.
+#define PM_RSTC         (MMIO_BASE + 0x0010001c)
+#define PM_WDOG         (MMIO_BASE + 0x00100024)
+#define MBOX_READ       (MMIO_BASE + 0xb880 + 0x0)
+#define MBOX_POLL       (MMIO_BASE + 0xb880 + 0x10)
+#define MBOX_SENDER     (MMIO_BASE + 0xb880 + 0x14)
+#define MBOX_STATUS     (MMIO_BASE + 0xb880 + 0x18)
+#define MBOX_CONFIG     (MMIO_BASE + 0xb880 + 0x1C)
+#define MBOX_WRITE      (MMIO_BASE + 0xb880 + 0x20)
+
+//
+//  Mailbox - An interface to communicate with VideoCore (GPU) on Raspberry Pi.
+//  https://github.com/raspberrypi/firmware/wiki/Mailboxes
+//
+
+// Mailbox channels.
+#define MBOX_CH_PROP    8
+
+// Mailbox tags.
+#define MBOX_TAG_SETCLKRATE  0x38002
+#define MBOX_TAG_LAST        0
+
+#define MBOX_CODE_REQUEST 0
+#define MBOX_FULL    0x80000000
+#define MBOX_EMPTY   0x40000000
 
 // https://www.raspberrypi.org/documentation/hardware/raspberrypi/bcm2836/QA7_rev3.4.pdf
 #define CORE0_TIMER_IRQCNTL 0x40000040
