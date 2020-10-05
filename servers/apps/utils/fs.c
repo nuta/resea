95bf26bb5e7e3b17f4732786f374797b55d8655e
Seiya Nuta <nuta@seiya.me>
2020-10-05 20:14:18 +0900

    utils: Add fs-write command

--- servers/apps/utils/fs.c
+++ servers/apps/utils/fs.c
@@ -39,3 +39,32 @@ void fs_read(const char *path) {
         offset += PAGE_SIZE;
     }
 }
+
+void fs_write(const char *path, const uint8_t *buf, size_t len) {
+    task_t fs_server = ipc_lookup("fs");
+    struct message m;
+    m.type = FS_CREATE_MSG;
+    m.fs_create.path = (char *) path;
+    m.fs_create.exist_ok = true;
+    error_t err = ipc_call(fs_server, &m);
+    if (IS_ERROR(err)) {
+        WARN("failed to create a file: '%s' (%s)", path, err2str(err));
+        return;
+    }
+
+    ASSERT(m.type == FS_CREATE_REPLY_MSG);
+    handle_t handle = m.fs_create_reply.handle;
+
+    m.type = FS_WRITE_MSG;
+    m.fs_write.handle = handle;
+    m.fs_write.offset = 0;
+    m.fs_write.data = buf;
+    m.fs_write.data_len = len;
+    err = ipc_call(fs_server, &m);
+    if (IS_ERROR(err)) {
+        WARN("failed to write into a file: %s", err2str(err));
+        return;
+    }
+
+    ASSERT(m.type == FS_WRITE_REPLY_MSG);
+}
--- servers/apps/utils/main.c
+++ servers/apps/utils/main.c
@@ -5,16 +5,24 @@
 #include "fs.h"
 
 void main(const char *cmdline) {
-    char *cmd, *url, *path;
+    char *cmd, *url, *path, *data;
     cmdline_cmd("http-get");
     cmdline_arg(&url, "http-get", "url", false);
+
     cmdline_cmd("fs-read");
     cmdline_arg(&path, "fs-read", "path", false);
+
+    cmdline_cmd("fs-write");
+    cmdline_arg(&path, "fs-write", "path", false);
+    cmdline_arg(&data, "fs-write", "data", false);
+
     cmdline_parse(cmdline, &cmd);
 
     if (!strcmp(cmd, "http-get")) {
         http_get(url);
     } else if (!strcmp(cmd, "fs-read")) {
         fs_read(path);
+    } else if (!strcmp(cmd, "fs-write")) {
+        fs_write(path, (uint8_t *) data, strlen(data));
     }
 }
