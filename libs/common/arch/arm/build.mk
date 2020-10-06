25e12ca0fd5b033198737934fbec62da056797cf
Seiya Nuta <nuta@seiya.me>
2020-10-06 20:30:22 +0900

    arm: Use -Os to reduce the .bss consumption

--- libs/common/arch/arm/build.mk
+++ libs/common/arch/arm/build.mk
@@ -1,3 +1,3 @@
 # Because the ROM and RAM spaces are too small, we override some cflags
 # to reduce the footprint.
-global-cflags-y += -Oz -flto -fno-sanitize=undefined
+global-cflags-y += -Os -flto -fno-sanitize=undefined
