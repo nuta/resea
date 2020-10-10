a8b304d60e3e5939c8c4117a611fe99511d0c76e
Seiya Nuta <nuta@seiya.me>
2020-10-10 22:15:59 +0900

    build: Use $(CONFIG_MACHINE_*) for machine-specific objs

--- kernel/arch/arm/machines/microbit/build.mk
+++ kernel/arch/arm/machines/microbit/build.mk
@@ -1,1 +1,1 @@
-objs-y += boot.o peripherals.o
+objs-$(CONFIG_MACHINE_MICROBIT) += boot.o peripherals.o
--- kernel/arch/arm64/machines/raspi3/build.mk
+++ kernel/arch/arm64/machines/raspi3/build.mk
@@ -1,1 +1,1 @@
-objs-y += boot.o peripherals.o
+objs-$(CONFIG_MACHINE_RASPI3) += boot.o peripherals.o
