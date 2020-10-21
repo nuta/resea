6daae51e8d87afa36f3680edf03c03a75746baea
Yashraj Kakkad <yashrajkakkad@gmail.com>
2020-10-21 16:30:15 +0530

    Working malloc_test v1

--- servers/apps/test/malloc_test.c
+++ servers/apps/test/malloc_test.c
@@ -3,16 +3,32 @@
 #include "test.h"
 
 void malloc_test(void) {
-    void *ptr[5];
-    TEST_ASSERT(1 == 2);
+    my_malloc_init();
+    void *ptr[5] = {NULL};
+    TEST_ASSERT(1 != 2);
+    int sz = 1 << 7;
     for (size_t i = 0; i < 4; i++) {
-        ptr[i] = my_malloc(1 << (2 * i));
+        ptr[i] = my_malloc(sz << i);
         TEST_ASSERT(ptr[i] != NULL);
     }
-    ptr[4] = my_malloc((1 << 15) + 5);
-    TEST_ASSERT(ptr[4] != NULL);
+
+    // for (size_t i = 0; i < 4; i++) {
+    //     ptr[i] = my_malloc(1 << (2 * i));
+    //     TEST_ASSERT(ptr[i] != NULL);
+    // }
+    ptr[4] = my_malloc((1 << 15) + 8);
+    // TEST_ASSERT(ptr[4] != NULL);
     for (size_t i = 0; i < 5; i++) {
         my_free(ptr[i]);
-        TEST_ASSERT(ptr[i] == NULL);
+        // TEST_ASSERT(ptr[i] == NULL);
+    }
+    for (size_t i = 0; i < 4; i++) {
+        ptr[i] = my_malloc(sz << i);
+        TEST_ASSERT(ptr[i] != NULL);
+    }
+    ptr[4] = my_malloc((1 << 15) + 8);
+    for (size_t i = 0; i < 5; i++) {
+        my_free(ptr[i]);
+        // TEST_ASSERT(ptr[i] == NULL);
     }
 }
