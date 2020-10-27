8461bc7dc0f02cda6c337adaa542705cc78313fd
Yashraj Kakkad <yashrajkakkad@gmail.com>
2020-10-27 19:04:35 +0530

    Integrated malloc, seems to work

--- libs/resea/build.mk
+++ libs/resea/build.mk
@@ -1,5 +1,5 @@
 name := resea
-objs-y += init.o printf.o malloc.o handle.o async.o task.o ipc.o timer.o mymalloc.o
+objs-y += init.o printf.o malloc.o handle.o async.o task.o ipc.o timer.o
 objs-y += cmdline.o
 global-cflags-y += -I$(dir)/arch/$(ARCH)
 subdirs-y += arch/$(ARCH)
