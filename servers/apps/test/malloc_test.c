9d819f579ea20225709bb874cfd06f90a71737b0
Yashraj Kakkad <yashrajkakkad@gmail.com>
2020-10-27 19:12:25 +0530

    my_ prefix removed from test

--- servers/apps/test/malloc_test.c
+++ servers/apps/test/malloc_test.c
@@ -1,11 +1,11 @@
-#include <resea/mymalloc.h>
+#include <resea/malloc.h>
 #include <resea/printf.h>
 #include <string.h>
 #include "test.h"
 #define NUM_PTRS 16
 
 void malloc_test(void) {
-    my_malloc_init();
+    malloc_init();
     void *ptr[NUM_PTRS];
     memset(ptr, 0, sizeof(ptr));
     TEST_ASSERT(
@@ -13,27 +13,27 @@ void malloc_test(void) {
     int sz = 1;
 
     for (size_t i = 0; i < NUM_PTRS - 1; i++) {
-        ptr[i] = my_malloc(sz << i);
+        ptr[i] = malloc(sz << i);
         TEST_ASSERT(ptr[i] != NULL);
     }
 
     // for (size_t i = 0; i < 4; i++) {
-    //     ptr[i] = my_malloc(1 << (2 * i));
+    //     ptr[i] = malloc(1 << (2 * i));
     //     TEST_ASSERT(ptr[i] != NULL);
     // }
-    ptr[NUM_PTRS - 1] = my_malloc((1 << 15) + 8);
+    ptr[NUM_PTRS - 1] = malloc((1 << 15) + 8);
     // TEST_ASSERT(ptr[4] != NULL);
     for (size_t i = 0; i < NUM_PTRS; i++) {
-        my_free(ptr[i]);
+        free(ptr[i]);
         // TEST_ASSERT(ptr[i] == NULL);
     }
     for (size_t i = 0; i < NUM_PTRS - 1; i++) {
-        ptr[i] = my_malloc(sz << i);
+        ptr[i] = malloc(sz << i);
         TEST_ASSERT(ptr[i] != NULL);
     }
-    ptr[NUM_PTRS - 1] = my_malloc((1 << 15) + 8);
+    ptr[NUM_PTRS - 1] = malloc((1 << 15) + 8);
     for (size_t i = 0; i < NUM_PTRS; i++) {
-        my_free(ptr[i]);
+        free(ptr[i]);
         // TEST_ASSERT(ptr[i] == NULL);
     }
 }
