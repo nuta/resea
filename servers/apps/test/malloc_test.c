b056117cbefd6fa150d4dd14eace720d3a79b572
Yashraj Kakkad <yashrajkakkad@gmail.com>
2020-11-06 16:12:44 +0530

    memset before free

--- servers/apps/test/malloc_test.c
+++ servers/apps/test/malloc_test.c
@@ -18,18 +18,24 @@ void malloc_test(void) {
 
     // Chunk corresponding to largest bin
     ptr[NUM_PTRS - 1] = malloc((1 << 15) + 8);
-    for (size_t i = 0; i < NUM_PTRS; i++) {
+    for (size_t i = 0; i < NUM_PTRS - 1; i++) {
+        memset(ptr[i], 0xaa, sz << i);
         free(ptr[i]);
     }
+    memset(ptr[NUM_PTRS - 1], 0xaa, (1 << 15) + 8);
+    free(ptr[NUM_PTRS - 1]);
 
     // Do the same allocation again and see if an existing chunk is allocated
     for (size_t i = 0; i < NUM_PTRS - 1; i++) {
         ptr[i] = malloc(sz << i);
         TEST_ASSERT(ptr[i] != NULL);
     }
-
     ptr[NUM_PTRS - 1] = malloc((1 << 15) + 8);
-    for (size_t i = 0; i < NUM_PTRS; i++) {
+
+    for (size_t i = 0; i < NUM_PTRS - 1; i++) {
+        memset(ptr[i], 0xaa, sz << i);
         free(ptr[i]);
     }
+    memset(ptr[NUM_PTRS - 1], 0xaa, (1 << 15) + 8);
+    free(ptr[NUM_PTRS - 1]);
 }
