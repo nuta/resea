b5fcb55433c00eb43bc9635021e19058ff85ce7d
Yashraj Kakkad <yashrajkakkad@gmail.com>
2020-11-09 15:23:09 +0530

    full stop after comments

--- servers/apps/test/malloc_test.c
+++ servers/apps/test/malloc_test.c
@@ -10,13 +10,13 @@ void malloc_test(void) {
     memset(ptr, 0, sizeof(ptr));
     int sz = 1;
 
-    // Allocate chunks of different sizes
+    // Allocate chunks of different sizes.
     for (size_t i = 0; i < NUM_PTRS - 1; i++) {
         ptr[i] = malloc(sz << i);
         TEST_ASSERT(ptr[i] != NULL);
     }
 
-    // Chunk corresponding to largest bin
+    // Chunk corresponding to largest bin.
     ptr[NUM_PTRS - 1] = malloc((1 << 15) + 8);
     TEST_ASSERT(ptr[NUM_PTRS - 1] != NULL);
 
@@ -27,7 +27,7 @@ void malloc_test(void) {
     memset(ptr[NUM_PTRS - 1], 0xaa, (1 << 15) + 8);
     free(ptr[NUM_PTRS - 1]);
 
-    // Do the same allocation again and see if an existing chunk is allocated
+    // Do the same allocation again and see if an existing chunk is allocated.
     for (size_t i = 0; i < NUM_PTRS - 1; i++) {
         ptr[i] = malloc(sz << i);
         TEST_ASSERT(ptr[i] != NULL);
