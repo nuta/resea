b0ec70b436b764c480f95b98cfa0ce3fed7ed028
PrayagS <prayag.s@ahduni.edu.in>
2020-11-11 01:45:33 +0530

    Update RTC driver

--- servers/drivers/time/rtc/main.c
+++ servers/drivers/time/rtc/main.c
@@ -8,11 +8,57 @@
 
 static io_t rtc_io;
 
+int is_update_in_progress(void) {
+	io_write8(rtc_io, 0, 10);
+    char RTC_UIP_FLAG = io_read8(rtc_io, 1);
+	return RTC_UIP_FLAG & 0x80;
+}
+
+uint8_t read_cmos_register(int reg_index) {
+    io_write8(rtc_io, 0, reg_index);
+    return (uint8_t) io_read8(rtc_io, 1);
+}
+
+uint8_t bcd_to_binary(uint8_t bcd) {
+    return (bcd & 0x0F) + ((bcd >> 4) * 10);
+}
+
+void read_datetime(uint8_t* year, uint8_t* month, uint8_t* day, uint8_t* hour, uint8_t* minute, uint8_t* second, uint8_t* day_of_week) {
+
+    // Wait until rtc is not updating
+    while (is_update_in_progress());
+
+    *second = read_cmos_register(0x00);
+    *minute = read_cmos_register(0x02);
+    *hour = read_cmos_register(0x04);
+    *day_of_week = read_cmos_register(0x06);
+    *day = read_cmos_register(0x07);
+    *month = read_cmos_register(0x08);
+    *year = read_cmos_register(0x09);
+
+    uint8_t status_b = read_cmos_register(0x0B);
+    bool use_BCD = !(status_b & 0x04);
+    bool use_twelve_hours = !(status_b & 0x02);
+
+    if(use_BCD) {
+        *second = bcd_to_binary(*second);
+        *minute = bcd_to_binary(*minute);
+        *hour = bcd_to_binary(*hour & 0x7F);
+        *day_of_week = bcd_to_binary(*day_of_week);
+        *day = bcd_to_binary(*day);
+        *month = bcd_to_binary(*month);
+        *year = bcd_to_binary(*year);
+    }
+
+    if(use_twelve_hours && (*hour & 0x80)) {
+        *hour =(((*hour & 0x7F) + 12) % 24);
+    }
+}
 
 void main(void) {
     rtc_io = io_alloc_port(RTC_PORT_IDX, 0x10, IO_ALLOC_NORMAL);
 
-    ASSERT_OK(ipc_serve("time"));
+    ASSERT_OK(ipc_serve("rtc"));
 
     TRACE("ready");
     while (true) {
@@ -22,21 +68,18 @@ void main(void) {
         ASSERT_OK(err);
 
         switch (m.type) {
-            case TIME_READ_MSG: {
-                char *format = m.time_read.format;
+            case TIME_READRTC_MSG: {
+                char *format = m.time_readrtc.format;
                 DBG("Received format -> %s", format);
-                m.type = TIME_READ_REPLY_MSG;
-                io_write8(rtc_io, 0, 0);
-                char sec = io_read8(rtc_io, 1);
-                io_write8(rtc_io, 0, 2);
-                char min = io_read8(rtc_io, 1);
-                io_write8(rtc_io, 0, 4);
-                char hour = io_read8(rtc_io, 1);
+
+                uint8_t year, month, day, hour, minute, second, day_of_week;
+                read_datetime(&year, &month, &day, &hour, &minute, &second, &day_of_week);
+
+                m.type = TIME_READRTC_REPLY_MSG;
                 int buf_size = 1024;
                 char *buf = malloc(buf_size);
-                snprintf(buf, buf_size, "%02x:%02x:%02x", hour, min, sec);
-                // DBG("TIME -> %02x:%02x:%02x", hour, min, sec);
-                m.time_read_reply.currenttime = buf;
+                snprintf(buf, buf_size, "%d %d/%d/%d %d:%d:%d", day_of_week, day, month, year, hour, minute, second);
+                m.time_readrtc_reply.currenttime = buf;
                 ipc_reply(m.src, &m);
                 break;
             }
@@ -44,5 +87,4 @@ void main(void) {
                 TRACE("unknown message %d", m.type);
         }
     }
-
 }
--- servers/drivers/time/rtc/rtc.h
+++ servers/drivers/time/rtc/rtc.h
@@ -2,6 +2,5 @@
 #define __RTC_H__
 
 #define RTC_PORT_IDX  0x70
-#define RTC_PORT_DATA 0x71
 
 #endif
