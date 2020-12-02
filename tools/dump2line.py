25769c8edf26f4ab984702ddd233e12792c7add0
Seiya Nuta <nuta@seiya.me>
2020-12-03 04:03:44 +0900

    dump2line.py: Support arbitrary hex strings

--- tools/dump2line.py
+++ tools/dump2line.py
@@ -6,24 +6,21 @@ import re
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Backtraces a QEMU memory dump (e.g. x /100g $esp)")
+        description="Try applying adddr2line to arbitrary hex strings in stdin.")
     parser.add_argument("--addr2line", default="gaddr2line", help="addr2line")
     parser.add_argument("--image", default="build/resea.elf",
         help="The executable.")
     args = parser.parse_args()
 
     for line in sys.stdin.readlines():
-        m = re.match(r"^([0-9a-f]+):", line)
-        if m:
-            _, addrs = line.split(": ")
-            for addr in addrs.split(" "):
-                addr = addr.strip()
-                stdout = subprocess.check_output(
-                    [args.addr2line, "-e", args.image, addr]) \
-                        .decode("utf-8").strip()
-                if stdout.startswith("??"):
-                    continue
-                print(f"{addr} {stdout}")
+        for addr in re.findall(r"\W(?P<addr>[0-9a-f]{8,})\W", line):
+            addr = addr.strip()
+            stdout = subprocess.check_output(
+                [args.addr2line, "-e", args.image, addr]) \
+                    .decode("utf-8").strip()
+            print(f"{addr} {stdout}")
+            if stdout.startswith("??"):
+                continue
 
 if __name__ == "__main__":
     main()
