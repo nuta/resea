f38eed03eb8565f9bb500dac939eaad2c49e35c1
Seiya Nuta <nuta@seiya.me>
2021-01-18 21:28:44 +0900

    Fix for code style consistency. NFC

--- servers/vm/shm.c
+++ servers/vm/shm.c
@@ -5,12 +5,14 @@
 
 static struct shm shared_mems[NUM_SHARED_MEMS_MAX];
 
-int shm_check_available() {
+int shm_check_available(void) {
     int i;
-    for (i = 0; i < NUM_SHARED_MEMS_MAX && shared_mems[i].inuse; i++)
-        ;
-    if (i == NUM_SHARED_MEMS_MAX)
+    for (i = 0; i < NUM_SHARED_MEMS_MAX && shared_mems[i].inuse; i++);
+    
+    if (i == NUM_SHARED_MEMS_MAX) {
         return -1;
+    }
+
     return i;
 }
 
@@ -19,12 +21,14 @@ error_t shm_create(struct task* task, size_t size, int* slot) {
     if (slot < 0) {
         return ERR_UNAVAILABLE;
     }
+
     paddr_t paddr = 0;
     vaddr_t vaddr;
     error_t err = task_page_alloc(task, &vaddr, &paddr, size);
     if (err != OK) {
         return err;
     }
+
     shared_mems[*slot].inuse = true;
     shared_mems[*slot].shm_id = *slot;
     shared_mems[*slot].len = size;
@@ -37,6 +41,7 @@ error_t shm_map(struct task* task, int shm_id, bool writable, vaddr_t* vaddr) {
     if (shm == NULL) {
         return ERR_NOT_FOUND;
     }
+    
     *vaddr = virt_page_alloc(task, shm->len);
     int flag = (writable) ? MAP_TYPE_READWRITE : MAP_TYPE_READONLY;
     error_t err = map_page(task, *vaddr, shm->paddr, flag, true);
