dd660237aff902231bfa7f4488b29ba2d66befc7
Seiya Nuta <nuta@seiya.me>
2021-08-21 16:13:23 +0900

    wip

--- servers/drivers/input/ps2/main.c
+++ servers/drivers/input/ps2/main.c
@@ -37,95 +37,13 @@ static void handle_keyboard_irq(void) {
           modifiers.shift_right ? " [shift-right]" : "");
 }
 
-static uint8_t read_status_reg(void) {
-    return io_read8(ps2_io, IOPORT_OFFSET_STATUS);
+static void wait_input(void) {
+    while ((io_read8(ps2_io, IOPORT_OFFSET_STATUS) & (1 << 1)) != 0)
+        ;
 }
 
-static uint8_t read_data_reg(void) {
-    return io_read8(ps2_io, IOPORT_OFFSET_DATA);
-}
-
-#define MOUSE_BYTE1_LEFT_BUTTON  (1 << 0)
-#define MOUSE_BYTE1_RIGHT_BUTTON (1 << 1)
-#define MOUSE_BYTE1_ALWAYS_1     (1 << 3)
-#define MOUSE_BYTE1_X_SIGNED     (1 << 4)
-#define MOUSE_BYTE1_Y_SIGNED     (1 << 5)
-#define MOUSE_BYTE1_X_OVERFLOW   (1 << 6)
-#define MOUSE_BYTE1_Y_OVERFLOW   (1 << 7)
-
-static void handle_mouse_irq(void) {
-    uint8_t status = read_status_reg();
-    bool mouse_input = (status & (1 << 5)) != 1;
-    if (!mouse_input) {
-        handle_keyboard_irq();
-        return;
-    }
-
-    int i = 0;
-    uint8_t data[3];
-    bool done = false;
-    while (!done) {
-        switch (i) {
-            case 0:
-                data[0] = read_data_reg();
-                if ((data[0] & MOUSE_BYTE1_ALWAYS_1) == 0) {
-                    // Invalid data, just ignore it.
-                    WARN("invalid first byte: data=%x, status=%x", data[0],
-                         status);
-                    done = true;
-                    break;
-                }
-                i++;
-                break;
-            case 1:
-                data[1] = read_data_reg();
-                i++;
-                break;
-            case 2: {
-                i = 0;
-                bool overflowed = (data[0] & MOUSE_BYTE1_X_OVERFLOW) != 0
-                                  || (data[0] & MOUSE_BYTE1_Y_OVERFLOW) != 0;
-                if (overflowed) {
-                    TRACE("overflowed (first_byte=%x)", data[0]);
-                    done = true;
-                    break;
-                }
-
-                data[2] = read_data_reg();
-                bool left_pressed = (data[0] & MOUSE_BYTE1_LEFT_BUTTON) != 0;
-                bool right_pressed = (data[0] & MOUSE_BYTE1_RIGHT_BUTTON) != 0;
-                int32_t x_raw = (data[0] & MOUSE_BYTE1_X_SIGNED)
-                                    ? (((uint32_t) data[1]) | 0xffffff00)
-                                    : data[1];
-                int32_t y_raw = (data[0] & MOUSE_BYTE1_Y_SIGNED)
-                                    ? (((uint32_t) data[2]) | 0xffffff00)
-                                    : data[2];
-                int x = x_raw;
-                int y = -y_raw;
-
-                DBG("mouse: data=[%x, %d, %d], x=%d, y=%d%s%s", data[0],
-                    data[1], data[2], x, y, left_pressed ? " [LEFT]" : "",
-                    right_pressed ? "[RIGHT]" : "");
-
-                struct message m;
-                m.type = MOUSE_INPUT_MSG;
-                m.mouse_input.x_delta = x;
-                m.mouse_input.y_delta = y;
-                m.mouse_input.clicked_left = left_pressed;
-                m.mouse_input.clicked_right = right_pressed;
-                ipc_send(gui_server, &m);
-                done = true;
-                break;
-            }
-            default:
-                UNREACHABLE();
-        }
-    }
-}
-
-/// Waits until the keyboard controller gets ready.
-static void wait_kbd(void) {
-    while ((io_read8(ps2_io, IOPORT_OFFSET_STATUS) & 0x02) != 0)
+static void wait_output(void) {
+    while ((io_read8(ps2_io, IOPORT_OFFSET_STATUS) & (1 << 0)) == 0)
         ;
 }
 
@@ -134,38 +52,131 @@ static void wait_kbd_ack(void) {
         ;
 }
 
+static void write_ps2_command(uint8_t cmd) {
+    wait_input();
+    io_write8(ps2_io, IOPORT_OFFSET_STATUS, cmd);
+}
+
+static void write_ps2_command_with_data(uint8_t cmd, uint8_t data) {
+    wait_input();
+    io_write8(ps2_io, IOPORT_OFFSET_STATUS, cmd);
+    wait_input();
+    io_write8(ps2_io, IOPORT_OFFSET_DATA, data);
+}
+
+static uint8_t read_ps2_response(void) {
+    wait_output();
+    return io_read8(ps2_io, IOPORT_OFFSET_DATA);
+}
+
+static uint8_t read_status_reg(void) {
+    return io_read8(ps2_io, IOPORT_OFFSET_STATUS);
+}
+
+static uint8_t read_data_reg(void) {
+    wait_output();
+    return io_read8(ps2_io, IOPORT_OFFSET_DATA);
+}
+
+static void handle_mouse_irq(void) {
+    static int state = 0;
+    static uint8_t data[3];
+
+    bool send_mouse_event = false;
+    int x = 0;
+    int y = 0;
+    bool left_pressed = false;
+    bool right_pressed = false;
+    while (true) {
+        uint8_t status = read_status_reg();
+        bool empty = (status & 1) == 0;
+        if (empty) {
+            break;
+        }
+
+        bool mouse_input = (status & (1 << 5)) != 0;
+        if (!mouse_input) {
+            handle_keyboard_irq();
+        } else {
+            switch (state) {
+                case 0:
+                    data[0] = read_data_reg();
+                    if ((data[0] & MOUSE_BYTE1_ALWAYS_1) == 0) {
+                        // Invalid data, just ignore it.
+                        TRACE("invalid first byte: data=%x, status=%x", data[0],
+                              status);
+                        break;
+                    }
+                    state++;
+                    break;
+                case 1:
+                    data[1] = read_data_reg();
+                    state++;
+                    break;
+                case 2: {
+                    state = 0;
+
+                    bool overflowed =
+                        (data[0] & MOUSE_BYTE1_X_OVERFLOW) != 0
+                        || (data[0] & MOUSE_BYTE1_Y_OVERFLOW) != 0;
+                    if (overflowed) {
+                        TRACE("overflowed (first_byte=%x)", data[0]);
+                        break;
+                    }
+
+                    data[2] = read_data_reg();
+                    left_pressed = (data[0] & MOUSE_BYTE1_LEFT_BUTTON) != 0;
+                    right_pressed = (data[0] & MOUSE_BYTE1_RIGHT_BUTTON) != 0;
+                    int32_t x_raw = (data[0] & MOUSE_BYTE1_X_SIGNED)
+                                        ? (((int) data[1]) - 0x100)
+                                        : data[1];
+                    int32_t y_raw = (data[0] & MOUSE_BYTE1_Y_SIGNED)
+                                        ? (((int) data[2]) - 0x100)
+                                        : data[2];
+                    x += x_raw;
+                    y += -y_raw;
+                    send_mouse_event = true;
+                    break;
+                }
+                default:
+                    UNREACHABLE();
+            }
+        }
+    }
+
+    if (send_mouse_event) {
+        TRACE("mouse: data=[%x, %d, %d], x=%d, y=%d%s%s", data[0], data[1],
+              data[2], x, y, left_pressed ? " [LEFT]" : "",
+              right_pressed ? "[RIGHT]" : "");
+
+        struct message m;
+        m.type = MOUSE_INPUT_MSG;
+        m.mouse_input.x_delta = x;
+        m.mouse_input.y_delta = y;
+        m.mouse_input.clicked_left = left_pressed;
+        m.mouse_input.clicked_right = right_pressed;
+        ipc_send(gui_server, &m);
+    }
+}
+
 static void init_device(void) {
     ps2_io = io_alloc_port(IOPORT_ADDR, IOPORT_SIZE, IO_ALLOC_NORMAL);
     ASSERT_OK(irq_acquire(MOUSE_IRQ));
     ASSERT_OK(irq_acquire(KEYBOARD_IRQ));
 
-    // Enable mouse.
-    wait_kbd();
-    io_write8(ps2_io, IOPORT_OFFSET_STATUS, 0xa8);
+    write_ps2_command(PS2_CMD_ENABLE_MOUSE);
 
     while (io_read8(ps2_io, IOPORT_OFFSET_STATUS) & KBD_STATUS_OUTBUF_FULL) {
         io_read8(ps2_io, IOPORT_OFFSET_DATA);
     }
-    wait_kbd();
-    io_write8(ps2_io, IOPORT_OFFSET_STATUS, 0x20);
-    wait_kbd();
-    uint8_t compaq_status = io_read8(ps2_io, IOPORT_OFFSET_DATA);
-    wait_kbd();
-    io_write8(ps2_io, IOPORT_OFFSET_STATUS, 0x60);
-    wait_kbd();
-    io_write8(ps2_io, IOPORT_OFFSET_DATA, compaq_status | 2);
 
-    wait_kbd();
-    io_write8(ps2_io, IOPORT_OFFSET_STATUS, 0xd4);
-    wait_kbd();
-    io_write8(ps2_io, IOPORT_OFFSET_DATA, 0xf6);
-    wait_kbd_ack();
+    write_ps2_command(PS2_CMD_READ_CONFIG);
+    uint8_t config = read_ps2_response();
+    write_ps2_command_with_data(PS2_CMD_WRITE_CONFIG,
+                                config | PS2_CONFIG_ENABLE_MOUSE_IRQ);
 
-    wait_kbd();
-    io_write8(ps2_io, IOPORT_OFFSET_STATUS, 0xd4);
-    wait_kbd();
-    io_write8(ps2_io, IOPORT_OFFSET_DATA, 0xf4);
-    wait_kbd_ack();
+    write_ps2_command_with_data(PS2_CMD_MOUSE_WRITE, MOUSE_CMD_SET_DEFAULTS);
+    write_ps2_command_with_data(PS2_CMD_MOUSE_WRITE, MOUSE_CMD_DATA_ON);
 }
 
 void main(void) {
--- servers/drivers/input/ps2/ps2.h
+++ servers/drivers/input/ps2/ps2.h
@@ -12,6 +12,23 @@
 #define KBD_STATUS_OUTBUF_FULL     (1 << 0)
 #define KBD_STATUS_AUX_OUTPUT_FULL (1 << 5)
 
+#define PS2_CMD_READ_CONFIG    0x20
+#define PS2_CMD_WRITE_CONFIG   0x60
+#define PS2_CMD_ENABLE_MOUSE   0xa8
+#define PS2_CMD_MOUSE_WRITE    0xd4
+#define MOUSE_CMD_SET_DEFAULTS 0xf6
+#define MOUSE_CMD_DATA_ON      0xf4
+
+#define PS2_CONFIG_ENABLE_MOUSE_IRQ (1 << 1)
+
+#define MOUSE_BYTE1_LEFT_BUTTON  (1 << 0)
+#define MOUSE_BYTE1_RIGHT_BUTTON (1 << 1)
+#define MOUSE_BYTE1_ALWAYS_1     (1 << 3)
+#define MOUSE_BYTE1_X_SIGNED     (1 << 4)
+#define MOUSE_BYTE1_Y_SIGNED     (1 << 5)
+#define MOUSE_BYTE1_X_OVERFLOW   (1 << 6)
+#define MOUSE_BYTE1_Y_OVERFLOW   (1 << 7)
+
 struct modifiers_state {
     bool ctrl_left;
     bool ctrl_right;
