68f906fed03cbac87100c3758309a21c2a57c7a0
Seiya Nuta <nuta@seiya.me>
2022-08-28 21:28:43 +0900

    Update

--- libs/common/include/types.h
+++ libs/common/include/types.h
@@ -10,6 +10,19 @@ typedef unsigned short uint16_t;
 typedef unsigned uint32_t;
 typedef unsigned long long uint64_t;
 
+#define INT8_MIN   -128
+#define INT16_MIN  -32768
+#define INT32_MIN  -2147483648
+#define INT64_MIN  -9223372036854775808LL
+#define INT8_MAX   127
+#define INT16_MAX  32767
+#define INT32_MAX  2147483647
+#define INT64_MAX  9223372036854775807LL
+#define UINT8_MAX  255
+#define UINT16_MAX 65535
+#define UINT32_MAX 4294967295U
+#define UINT64_MAX 18446744073709551615ULL
+
 #ifdef __LP64__
 typedef int64_t intmax_t;
 typedef uint64_t uintmax_t;
@@ -17,6 +30,11 @@ typedef uint64_t uintmax_t;
 typedef int32_t intmax_t;
 typedef uint32_t uintmax_t;
 #endif
+
+typedef char bool;
+#define true 1
+#define false 0
+#define NULL ((void *) 0)
 
 typedef int task_t;
 
