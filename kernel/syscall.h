aa09552ae95c9605c85dac28a753c0716f34fd6b
Seiya Nuta <nuta@seiya.me>
2022-08-28 18:12:31 +0900

    Update

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -1,26 +1,36 @@
-#define __failable
 #define __user
+typedef unsigned char uint8_t;
 typedef int task_t;
 typedef int error_t;
 typedef int uaddr_t;
 typedef int paddr_t;
 typedef int notifications_t;
+typedef int caps_t;
 
 struct message {
+    unsigned tag;
+    task_t src;
+    union {
+        uint8_t data[56];
+
+    };
 };
 
-__failable task_t task_create(uaddr_t ip, uaddr_t name, task_t pager, unsigned flags);
+task_t task_create(uaddr_t ip, uaddr_t name, task_t pager, unsigned flags);
 error_t task_destroy(task_t task);
 error_t task_exit(void);
-__failable task_t task_self(void);
+task_t task_self(void);
 
-error_t ipc(task_t dst, task_t src, __user struct message *m, unsigned flags);
+caps_t caps_get(task_t task);
+error_t caps_set(task_t task, caps_t caps);
+
+task_t ipc(task_t dst, task_t src, __user struct message *m, unsigned flags);
 error_t notify(task_t dst, notifications_t notifications);
 
 error_t timer_set(unsigned ms);
 error_t timer_clear(unsigned ms);
 
-__failable paddr_t pm_alloc(int num_pages);
+paddr_t pm_alloc(int num_pages);
 error_t pm_free(paddr_t paddr, int num_pages);
 
 error_t vm_map(task_t task, uaddr_t uaddr, paddr_t paddr, int num_pages);
@@ -29,5 +39,7 @@ error_t vm_unmap(task_t task, uaddr_t uaddr, int num_pages);
 error_t irq_acquire(int irq);
 error_t irq_release(int irq);
 
-__failable int sys_console_read(uaddr_t buf, int buf_len);
-__failable int sys_console_write(uaddr_t buf, int len);
+int console_read(uaddr_t buf, int buf_len);
+int console_write(uaddr_t buf, int len);
+
+int kdebug(uaddr_t cmd, uaddr_t out, int out_len);
