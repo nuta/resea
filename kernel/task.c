dc852eb2f9526c6c42c0c303d5c7be8c0fd106de
Seiya Nuta <nuta@seiya.me>
2022-08-30 19:15:26 +0900

    Update

--- kernel/task.c
+++ kernel/task.c
@@ -1,6 +1,44 @@
 #include "task.h"
+#include "arch.h"
+#include <list.h>
 
-void task_switch(struct task *next) {
-    struct task *current = CURRENT_TASK;
-    arch_task_switch(current, next);
+static list_t runqueues[TASK_PRIORITY_MAX];
+
+static void enqueue_task(struct task *task) {
+    list_push_back(&runqueues[task->priority], &task->next);
 }
+
+static struct task *scheduler(struct task *current) {
+    if (current != IDLE_TASK && current->state == TASK_RUNNABLE) {
+        // The current task is still runnable. Enqueue into the runqueue.
+        enqueue_task(current);
+    }
+
+    // Look for the task with the highest priority. Tasks with the same priority
+    // is scheduled in round-robin fashion.
+    for (int i = 0; i < TASK_PRIORITY_MAX; i++) {
+        struct task *next = LIST_POP_FRONT(&runqueues[i], struct task, next);
+        if (next) {
+            return next;
+        }
+    }
+
+    return IDLE_TASK;
+}
+
+void task_switch(void) {
+    struct task *prev = CURRENT_TASK;
+    struct task *next = scheduler(prev);
+    DEBUG_ASSERT(next != prev);
+
+    CURRENT_TASK = next;
+    arch_task_switch(prev, next);
+}
+
+/// Initializes the task subsystem.
+void task_init(void) {
+    for (int i = 0; i < TASK_PRIORITY_MAX; i++) {
+        list_init(&runqueues[i]);
+    }
+}
+
--- kernel/task.h
+++ kernel/task.h
@@ -3,16 +3,20 @@
 #include <list.h>
 #include <arch_types.h>
 
+#define TASK_PRIORITY_MAX 8
 #define TASK_NAME_LEN 16
+
+#define TASK_RUNNABLE 1
+#define TASK_BLOCKED  2
 
 struct task {
     /// The arch-specific fields.
     struct arch_task arch;
     task_t tid;
 
-    // state;
-    // priority;
-    // quantum;
+    int state;
+    int priority;
+    int quantum;
 
     // struct message m;
     // notifications_t notifications;
