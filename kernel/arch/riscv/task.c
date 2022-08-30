a649bbce7b81b3f69d7b349d1329dae1777c76b4
Seiya Nuta <nuta@seiya.me>
2022-08-30 20:52:27 +0900

    Update

--- kernel/arch/riscv/task.c
+++ kernel/arch/riscv/task.c
@@ -1,8 +1,7 @@
+#include "switch.h"
 #include <kernel/arch.h>
 #include <kernel/task.h>
 
-void riscv_switch(uint32_t *prev_sp, uint32_t *next_sp);
-
 void arch_task_switch(struct task *prev, struct task *next) {
-    riscv_task_switch(prev->arch.sp, next->arch.sp);
+    riscv_task_switch(&prev->arch.sp, &next->arch.sp);
 }
