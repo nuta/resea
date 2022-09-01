a0429d0f05d9340a3d47484a5d5680af6290e28f
Seiya Nuta <nuta@seiya.me>
2022-09-02 02:31:28 +0900

    Update

--- kernel/arch/riscv32/setup.c
+++ kernel/arch/riscv32/setup.c
@@ -20,14 +20,55 @@ __noreturn static void setup_smode(void) {
     UNREACHABLE();
 }
 
+struct risc32_trap_frame {
+    uint32_t ra;
+    uint32_t gp;
+    uint32_t t0;
+    uint32_t t1;
+    uint32_t t2;
+    uint32_t s0;
+    uint32_t s1;
+    uint32_t a0;
+    uint32_t a1;
+    uint32_t a2;
+    uint32_t a3;
+    uint32_t a4;
+    uint32_t a5;
+    uint32_t a6;
+    uint32_t a7;
+    uint32_t s2;
+    uint32_t s3;
+    uint32_t s4;
+    uint32_t s5;
+    uint32_t s6;
+    uint32_t s7;
+    uint32_t s8;
+    uint32_t s9;
+    uint32_t s10;
+    uint32_t s11;
+    uint32_t t3;
+    uint32_t t4;
+    uint32_t t5;
+    uint32_t t6;
+    uint32_t sp;
+    uint32_t sepc;
+    uint32_t sstatus;
+} __packed;
+
 void riscv32_trap_handler(void);
-void riscv32_trap(void) {
+void riscv32_trap(struct risc32_trap_frame *frame) {
     uint32_t tp;
     __asm__ __volatile__("mv %0, tp" : "=r"(tp));
     uint32_t sscratch;
     __asm__ __volatile__("csrr %0, sscratch" : "=r"(sscratch));
-    TRACE("trap sepc=%p, scause=%p, tp=%p, sscratch=%p", read_sepc(),
-          read_scause(), tp, sscratch);
+
+    uint32_t scause = read_scause();
+    if (scause == 8) {
+        frame->sepc += 4;
+    }
+
+    TRACE("trap sepc=%p, scause=%p, tp=%p, sscratch=%pm next_sepc=%p",
+          read_sepc(), read_scause(), tp, sscratch, frame->sepc);
 }
 
 __noreturn void riscv32_setup(void) {
