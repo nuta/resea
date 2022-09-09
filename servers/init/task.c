4a690fb269b3bbe9347d0e0d82a2fb2d64715a98
Seiya Nuta <nuta@seiya.me>
2022-09-09 23:20:15 +0900

    Update

--- servers/init/task.c
+++ servers/init/task.c
@@ -53,7 +53,12 @@ task_t task_spawn(struct bootfs_file *file, const char *cmdline) {
     task->pager = task_self();
     task->ehdr = ehdr;
     task->phdrs = (elf_phdr_t *) ((uintptr_t) file_header + ehdr->e_phoff);
-    task->name[0] = '\0';  // FIXME: copy
+
+    int i = 0;
+    for (; i < sizeof(task->name) - 1 && file->name[i] != '\0'; i++) {
+        task->name[i] = file->name[i];
+    }
+    task->name[i] = '\0';
 
     tasks[task->tid - 1] = task;
     return task->tid;
