d69d2efffdd9c766f9ccc2b264e3e6b3366c06bc
Seiya Nuta <nuta@seiya.me>
2022-09-10 17:51:09 +0900

    Update

--- libs/common/arch/riscv32/backtrace.c
+++ libs/common/arch/riscv32/backtrace.c
@@ -23,7 +23,7 @@ void backtrace(void) {
     for (int i = 0; fp && i < BACKTRACE_MAX; i++) {
         struct stack_frame *frame =
             (struct stack_frame *) (fp - sizeof(*frame));
-        TRACE("{{{bt:%d:0x%p:ra}}}", i, frame->ra);
+        WARN("{{{bt:%d:0x%p:ra}}}", i, frame->ra);
         fp = frame->fp;
     }
 }
