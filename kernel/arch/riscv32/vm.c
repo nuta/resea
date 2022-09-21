66b7064c757552c31a8847d65bae1e91932ffa10
Seiya Nuta <nuta@seiya.me>
2022-09-20 22:05:51 -0700

    Update

--- kernel/arch/riscv32/vm.c
+++ kernel/arch/riscv32/vm.c
@@ -65,6 +65,7 @@ error_t arch_vm_map(struct arch_vm *vm, vaddr_t vaddr, paddr_t paddr,
                              page_attrs_to_pte_flags(attrs) | PTE_V);
     }
 
+    // TODO: move sfence.vma into the above loop
     for (uint32_t offset = 0; offset < size; offset += PAGE_SIZE) {
         asm_sfence_vma(vaddr + offset);
     }
